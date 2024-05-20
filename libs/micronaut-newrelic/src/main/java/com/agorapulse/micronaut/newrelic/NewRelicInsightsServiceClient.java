bd8ff31ec56f0107b550e2d740a66ef76f58a7e3
musketyr <vladimir@orany.cz>
2024-05-20 16:24:43 +0200

    Added Retryable to HTTP clients

--- libs/micronaut-newrelic/src/main/java/com/agorapulse/micronaut/newrelic/NewRelicInsightsServiceClient.java
+++ libs/micronaut-newrelic/src/main/java/com/agorapulse/micronaut/newrelic/NewRelicInsightsServiceClient.java
@@ -24,9 +24,11 @@ import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.retry.annotation.Retryable;
 
 import java.util.Map;
 
+@Retryable
 @NewRelicInsights
 @Client("newrelic-insights")
 @Requires(property = "micronaut.http.services.newrelic-insights.url", classes = HttpClient.class, beans = NewRelicInsightsClientFilter.class)
